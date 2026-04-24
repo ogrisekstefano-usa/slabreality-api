fa3edb1af3dc62a7fbdf2627f9d40db0167ceb3e
ogrisekstefano-usa <ogrisek.stefano@gmail.com>
2026-04-24 13:51:38 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from supabase import create_client, Client
+import os
 from fastapi import FastAPI, UploadFile, File
 import boto3
 import os
@@ -5,11 +7,18 @@ import os
 app = FastAPI()
 
 # ENV VARIABLES (le mettiamo dopo su Railway)
+# R2
 R2_ACCESS_KEY = os.getenv("R2_ACCESS_KEY")
 R2_SECRET_KEY = os.getenv("R2_SECRET_KEY")
 R2_BUCKET = "slabreality"
 R2_ENDPOINT = os.getenv("R2_ENDPOINT")  # tipo https://xxxx.r2.cloudflarestorage.com
 R2_PUBLIC_URL = os.getenv("R2_PUBLIC_URL")  # tipo https://pub-xxx.r2.dev
+
+# SUPABASE
+SUPABASE_URL = "https://qzxcazacujjieafoqbrh.supabase.co"
+SUPABASE_KEY = "sb_publishable_7NZtJmgog-Z5FO_-Y39LxA_IV7YbPZW"
+
+supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
 
 s3 = boto3.client(
     "s3",
